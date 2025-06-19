3ce4eb77fe9f39d4948bd38e2f37f57004ce6dcb
FiXtal4 <adamadamob1202@gmail.com>
2025-06-19 20:45:46 +0300

    mini update

--- main.c
+++ main.c
@@ -1,32 +1,64 @@
 #include <string.h>
 #include <unistd.h>
+#define MAX 64
 
-int main()
+int tokenize_input(char *input, char *argv[], int max_args)
 {
-    char str[64];
+    char *p = input;
+    int i = 0;
 
-    while(1)
-    {
+    while(*p != '\0'){
+        while(*p == ' ') p++;
+
+        if (*p == '\0'){
+            break;
+        }
+        else{
+            argv[i] = p;
+
+            if (i >= max_args - 1){
+                break;
+            }
+
+            while (*p != ' ' && *p != '\0'){ 
+                p++;
+            }
+            if (*p == ' '){
+                    *p = '\0';
+                    p++;
+             }
+            }
+        i++;
+        }
+
+    argv[i] = NULL;
+
+    return i;
+}
+int main(){
+    char str[MAX];
+
+    while(1){
         write(1, "#: ", 3);
-
         ssize_t str_size = read(0, str, sizeof(str));
 
         if (str_size == 0){
             write(1, "\n", 1);
             break;
         }
-
-        str[str_size] = '\0'; 
-        
         if (str_size > 0 && str[str_size - 1] == '\n'){
             str[str_size - 1] = '\0';
         }
 
-        if (strcmp(str, "exit") == 0){
+        char *argv[MAX];
+        int argc = tokenize_input(str, argv, MAX);
+
+        if (argc == 0) continue;
+
+        if (strcmp(argv[0], "exit") == 0){
             break;
         }
-
-        write(1, str, strlen(str));
+        write(1, argv[0], strlen(argv[0]));
         write(1, "\n", 1);
     }
     return 0;
